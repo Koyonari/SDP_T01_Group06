f6a3fa5b7f2a9996081b020df27b392572f82cda
brazeenNP <s10255790@connect.np.edu.sg>
2025-01-30 11:47:53 +0800

    added initial template method classes

---
+++ Document.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_T01_Group06
+{
+	public abstract class Document
+	{
+		public void assembleDocument()
+		{
+			addHeader();
+			addFooter();
+			createBody();
+		}
+
+		public void addHeader()
+		{
+			Console.WriteLine("Header added to document");
+		}
+		public void addFooter()
+		{
+			Console.WriteLine("Footer added to document");
+		}
+
+		public abstract void createBody();
+	}
+}
---
+++ GrantProposal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDP_T01_Group06
+{
+	public class GrantProposal : Document
+	{
+		public override void createBody()
+		{
+			Console.WriteLine("Created Grant Proposal body");
+		}
+	}
+}
